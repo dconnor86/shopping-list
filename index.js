5fc1ce988ce20805befab5bd9011a582a70597fd
dconnor86 <46802000+dconnor86@users.noreply.github.com>
2019-02-19 19:41:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function registerEventHandler() {
+    $(".shopping-item-delete").click(function(e){
+        $(e.target).closest("li").remove();
+      });
+      
+    $(".shopping-item-toggle").click(function(e){
+        $(e.target).closest('li').toggleClass("shopping-item__checked");
+    })
+}
 
+
+$(document).ready(function(){
+ registerEventHandler();
+
+  
+  $("#js-shopping-list-form").submit (function(e){
+    var text = $('#shopping-list-entry').val();
+    var elm = `<li>
+    <span class="shopping-item">${text}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+    $('.shopping-list').append(elm).toggleClass("shopping-item")
+    e.preventDefault();
+    registerEventHandler();
+  })
+
+
+
+  
+});
